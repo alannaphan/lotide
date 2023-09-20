4b0e14bc6fe023d32058746ecc12a5a525fc0590
Alanna Phan <phan.alanna@gmail.com>
2023-09-19 23:26:05 -0400

    created assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+const assertArraysEqual = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+      return;
+    }
+  } console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
