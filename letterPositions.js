5a30ca0e817bf2507f669cb58b5ba256c1bfb8ac
Alanna <phan.alanna@gmail.com>
2023-09-20 19:36:04 -0400

    implemented letter positions.js

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === " ") {
+      continue;
+    }
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+const positions = letterPositions("lighthouse in the house");
+
+assertArraysEqual(positions["h"], [3, 5, 15, 18]); //true
+assertArraysEqual(positions["n"], [12]); //true
+assertArraysEqual(letterPositions("hello").e, [1]); //true
+assertArraysEqual(letterPositions("I am testing this hehe").e, [6, 19, 21]); //true
+
+
